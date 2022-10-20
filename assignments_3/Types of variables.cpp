80ebca18c3a1515cba6425db2af5f59131a3a82d
Ghaith-Zahda <115993715+Ghaith-Zahda@users.noreply.github.com>
2022-10-20 07:49:36 +0300

    Assignment-3
    Get to know the variables

---
+++ assignments_3/Types of variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    printf("%0.3f",d);
+    cout<<endl;
+    printf("%0.9f",e);    
+    return 0;
+}
