0f30db861965c432923d59a9d326b3c9d87d3688
Ghaith-Zahda <115993715+Ghaith-Zahda@users.noreply.github.com>
2022-10-29 12:35:24 +0200

    for loop
    Learn loops of repetition

---
+++ assignment-5/pro2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			if (i + j == 4 || i + j > 4 && j < 4 || i + 4 == j || i + 4 > j && j > 3) cout << "*";
+			else cout << " ";
+		} 
+		cout << endl;
+	}
+}
